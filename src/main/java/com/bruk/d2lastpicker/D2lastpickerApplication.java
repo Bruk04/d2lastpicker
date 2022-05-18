05a663f6eab2f60db02afdd45c1ef018b7fee4bf
Nick <iamnick2002@gmail.com>
2022-05-18 16:09:26 -0400

    Inital commit

---
+++ src/main/java/com/bruk/d2lastpicker/D2lastpickerApplication.java
@@ -0,0 +1,13 @@
+package com.bruk.d2lastpicker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class D2lastpickerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(D2lastpickerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bruk/d2lastpicker/D2lastpickerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bruk.d2lastpicker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class D2lastpickerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
